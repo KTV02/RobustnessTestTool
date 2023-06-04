efa72cf9ad27ed726b871faf429fd83a15a7eb47
KTV02 <l.kremp@gmx.de>
2023-06-04 21:23:46 +0200

    Fixed Dockerlist in GUI

--- webapp/frontend/src/components/gui.vue
+++ webapp/frontend/src/components/gui.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container">
     <div class="sidebar">
-      <div class="docker-list" v-if="dockerList.length > 0">
-        <div v-for="item in dockerList" :key="item.name" @click="selectContainer(item)">
-          {{ item.name }}
+      <div v-if="dockerListLoaded && dockerList.length > 0" class="docker-list">
+        <div v-for="(item, index) in dockerList" :key="item[1]" @click="selectContainer(item[1])" class="docker-item">
+          <div class="docker-item-text">{{ item[0] }}</div>
         </div>
       </div>
-      <div class="empty-list" v-else>
-        No Docker containers available
+      <div v-else class="empty-list">
+        {{ dockerListLoaded ? 'No Docker containers available' : 'Loading Docker containers...' }}
       </div>
     </div>
     <div class="main-panel">
@@ -24,7 +24,7 @@
           </div>
         </div>
         <div v-else>
-          <div class="no-results">No results present, run the tests</div>
+          <div class="no-results">No results present for {{ selectedContainer.name }}</div>
           <button class="run-tests-button" @click="runTests">Run Tests</button>
         </div>
       </div>
@@ -32,85 +32,140 @@
         Run tests to display results
       </div>
     </div>
+    <button class="add-docker-button" @click="openAddDockerDialog">Add Docker Container</button>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
       dockerList: [], // List of Docker containers
       selectedContainer: null, // Selected Docker container
       testResultsAvailable: false, // Boolean indicating if test results are available
+      dockerListLoaded: false,
       score: 0, // Robustness score
       labels: [], // Array of labels for the boxes
     };
   },
-  mounted() {
-    // Fetch initial Docker container list from the backend
+  created() {
+    // Execute loadDockerContainers method before creating the component
     this.loadDockerContainers();
   },
   methods: {
     async loadDockerContainers() {
       try {
-        const response = await this.$axios.get('/api/docker-containers');
+        const response = await this.$axios.get('/api/docker-containers'); // Update the URL with the correct backend URL
         this.dockerList = response.data;
+        this.dockerListLoaded = true; // Set the flag to indicate that the data has been loaded
+        console.log(this.dockerList)
+        console.log(this.dockerList.length)
+
       } catch (error) {
         console.error(error);
       }
     },
     selectContainer(container) {
-      // Set the selectedContainer data property to the clicked Docker container
-      // Make a request to the Python backend to check if test results are available for the selected container
-      // Update the testResultsAvailable, score, and labels data properties based on the received data
+      this.selectedContainer = container;
+      this.loadTestResults(container);
+    },
+    async loadTestResults(container) {
+      try {
+        const response = await this.$axios.post('/api/run-tests', {
+          container: container.name
+        }); // Update the URL with the correct backend URL and endpoint
+        this.testResultsAvailable = true;
+        this.score = response.data.score;
+        // Update other necessary data based on the received response
+      } catch (error) {
+        console.error(error);
+      }
     },
     runTests() {
       // Make a request to the Python backend to run tests for the selected Docker container
       // Update the testResultsAvailable, score, and labels data properties based on the received data
+    },
+    openAddDockerDialog() {
+      // Show a dialog for the user to provide a .tar file and container name
+      // Send the selected file and container name to the Python backend for processing
+      // Update the dockerList data property if the operation is successful
     },
   },
 };
 </script>
 
 <style scoped>
+
+
+.docker-item {
+  color: #000000;
+  display: flex;
+  align-items: center;
+  justify-content: center; /* Center the text horizontally */
+  padding: 10px 0; /* Add vertical padding for spacing */
+  border-bottom: 1px solid #ccc; /* Add border-bottom to all Docker items */
+}
+
+.docker-item:last-child {
+  border-bottom: none; /* Remove border-bottom for the last Docker item */
+}
+
+.docker-item-text {
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+  text-align: center; /* Center the text vertically */
+}
+
 .container {
   display: flex;
-  background-color: #f8f8f8; /* Change background color here */
 }
 
 .sidebar {
   width: 20%;
-  background-color: #fff; /* Change background color here */
+  background-color: #f2f2f2;
   padding: 20px;
 }
 
 .docker-list {
-  height: calc(100vh - 60px);
-  overflow-y: auto;
-}
-
-.docker-item {
-  color: blue; /* Change text color here */
-  margin-bottom: 10px;
-  cursor: pointer;
-}
-.empty-results {
-  color: black; /* Set the text color to a light color (e.g., white) */
+  margin-bottom: 20px;
 }
 
 .empty-list {
-  text-align: center;
-  color: black; /* Change text color here */
-  margin-top: 10px;
+  color: #999999;
 }
 
 .main-panel {
-  flex-grow: 1;
+  width: 80%;
   padding: 20px;
-  background-color: #ffffff; /* Change background color here */
 }
 
-/* Rest of the styles... */
+.empty-results {
+  color: #999999;
+  text-align: center;
+  font-size: 20px;
+}
 
+.no-results {
+  margin-bottom: 20px;
+}
+
+.run-tests-button {
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
+
+.add-docker-button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+}
 </style>
-
